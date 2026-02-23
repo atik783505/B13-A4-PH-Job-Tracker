58528303dc0ee8c660c0697473d0d311d1c51248
atik783505 <atik13672@gmail.com>
2026-02-24 02:11:19 +0600

    interview section complete

--- js/script.js
+++ js/script.js
@@ -33,6 +33,15 @@ function toggle(id) {
 
     selected.classList.add('btn-primary');
 
+    if (id == 'interview-btn') {
+        allcard.classList.add('hidden');
+        filteredSection.classList.remove('hidden');
+
+    } else if (id == 'all-btn') {
+        allcard.classList.remove('hidden');
+        filteredSection.classList.add('hidden');
+    }
+
 }
 
 maincontainer.addEventListener('click', function (event) {
@@ -42,16 +51,18 @@ maincontainer.addEventListener('click', function (event) {
         const jobType = parentNode.querySelector('.job-type').innerText;
         const jobInfo = parentNode.querySelector('.job-info').innerText;
         const jobStatus = parentNode.querySelector('.job-status').innerText;
+        const jobExperience = parentNode.querySelector('.job-experience').innerText;
+        parentNode.querySelector('.job-status').innerText = 'INTERVIEWED'
 
         const cardInfo = {
             companies,
             jobType,
             jobInfo,
-            jobStatus
+            jobStatus: 'INTERVIEWED',
+            jobExperience
         }
 
         const companiesExist = interveiwList.find(item => item.companies == cardInfo.companies);
-        parentNode.querySelector('.job-status').innerText = 'INTERVIEWED'
         if (!companiesExist) {
             interveiwList.push(cardInfo);
         }
@@ -64,23 +75,72 @@ function renderInterveiw() {
     filteredSection.innerHTML = '';
 
     for (let interveiws of interveiwList) {
-        console.log(interveiws)
         let div = document.createElement('div');
         div.className = "cards  bg-white rounded-lg p-6 flex justify-between border border-gray-300 border-l-8"
         div.innerHTML = `
                <div class="space-y-6">
                     <div>
-                        <p class="companies text-[18px] font-semibold text-[#002C5C] leading-[26px]">Mobile First Corp</p>
-                        <p class="job-type text-[#64748B] leading-[22px]">React Native Developer</p>
+                        <p class="companies text-[18px] font-semibold text-[#002C5C] leading-[26px]">${interveiws.companies}</p>
+                        <p class="job-type text-[#64748B] leading-[22px]">${interveiws.jobType}</p>
                     </div>
-                    <p class="job-info text-[#64748B] text-[14px] leading-5">Remote
-                        •
-                        Full-time
-                        •
-                        $130,000 - $175,000</p>
-                    <p class="job-status btn text-[#002C5C] font-medium leading-5 bg-[#EEF4FF]">Not Applied</p>
-                    <p>Build cross-platform mobile applications using React Native. Work on products used by millions of
-                        users worldwide.</p>
+                    <p class="job-info text-[#64748B] text-[14px] leading-5">${interveiws.jobInfo}</p>
+                    <p class="job-status btn text-[#002C5C] font-medium leading-5 bg-[#EEF4FF]">${interveiws.jobStatus}</p>
+                    <p>${interveiws.jobExperience}</p>
+                    <div class="flex gap-2">
+                        <button class="btn btn-interview border-green-500 text-green-500">INTERVIEW</button>
+                        <button class="btn btn-rejected border-red-600 text-red-600">REJECTED</button>
+                    </div>
+                </div>
+                <div>
+                    <button class="btn delete-btn rounded-full"><i class="fa-regular fa-trash-can"></i></button>
+                </div>
+        `
+        filteredSection.appendChild(div);
+    }
+}
+
+maincontainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('rejected-btn')) {
+        const parentNode = event.target.parentNode.parentNode;
+        const companies = parentNode.querySelector('.companies').innerText;
+        const jobType = parentNode.querySelector('.job-type').innerText;
+        const jobInfo = parentNode.querySelector('.job-info').innerText;
+        const jobStatus = parentNode.querySelector('.job-status').innerText;
+        const jobExperience = parentNode.querySelector('.job-experience').innerText;
+        parentNode.querySelector('.job-status').innerText = 'INTERVIEWED'
+
+        const cardInfo = {
+            companies,
+            jobType,
+            jobInfo,
+            jobStatus: 'REJECTED',
+            jobExperience
+        }
+
+        const companiesExist = rejectedList.find(item => item.companies == cardInfo.companies);
+        if (!companiesExist) {
+            rejectedList.push(cardInfo);
+        }
+        renderReject()
+    }
+
+})
+
+function renderReject() {
+    filteredSection.innerHTML = '';
+
+    for (let reject of rejectedList) {
+        let div = document.createElement('div');
+        div.className = "cards  bg-white rounded-lg p-6 flex justify-between border border-gray-300 border-l-8"
+        div.innerHTML = `
+               <div class="space-y-6">
+                    <div>
+                        <p class="companies text-[18px] font-semibold text-[#002C5C] leading-[26px]">${reject.companies}</p>
+                        <p class="job-type text-[#64748B] leading-[22px]">${reject.jobType}</p>
+                    </div>
+                    <p class="job-info text-[#64748B] text-[14px] leading-5">${reject.jobInfo}</p>
+                    <p class="job-status btn text-[#002C5C] font-medium leading-5 bg-[#EEF4FF]">${reject.jobStatus}</p>
+                    <p>${reject.jobExperience}</p>
                     <div class="flex gap-2">
                         <button class="btn btn-interview border-green-500 text-green-500">INTERVIEW</button>
                         <button class="btn btn-rejected border-red-600 text-red-600">REJECTED</button>
